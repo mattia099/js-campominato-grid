35835bd8931443ca8e57f19661107d5b67a541b6
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-02-15 14:34:49 +0100

    layout e griglia in base alla modalità

---
+++ jscript.js
@@ -0,0 +1,37 @@
+
+const buttonPlay = document.getElementById('play');
+const selectDifficult = document.getElementById('difficult');
+const grid = document.querySelector('.grid');
+
+buttonPlay.addEventListener('click', function(){
+
+    switch (selectDifficult.value){
+        case 'easy':
+            console.log(selectDifficult.value);
+            boxGenerator(100,10);
+            break;
+
+        case 'medium':
+            console.log(selectDifficult.value);
+            boxGenerator(81,9);
+            break;
+        
+        case 'crazy':
+            console.log(selectDifficult.value);
+            boxGenerator(49,7);
+            break;    
+    }
+
+})
+
+function boxGenerator(nBox , nCol){
+    for(let i=0; i<nBox; i++){
+        let box = document.createElement('div');
+        box.classList.add('square');
+        box.append(i+1);
+        box.style.width = `calc(100% / ${nCol})`
+        grid.appendChild(box);
+    }
+}
+
+
